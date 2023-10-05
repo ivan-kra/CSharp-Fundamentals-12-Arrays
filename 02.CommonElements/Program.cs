abd04569d6358f6965295e6dd02e24ab310e38b8
Ivan Kratchanov <ivankra@gmail.com>
2023-10-05 21:36:03 +0300

    Add files via upload

---
+++ 02.CommonElements/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] firstArr = Console.ReadLine().Split();
+string[] secondArr = Console.ReadLine().Split();
+
+for (int i = 0; i < secondArr.Length; i++)
+{
+    for (int j = 0; j < firstArr.Length; j++)
+    {
+        if (secondArr[i] == firstArr[j])
+        {
+            Console.Write($"{firstArr[j]} ");
+        }
+    }
+}
---
+++ 03.Zig-ZagArrays/Program.cs
@@ -0,0 +1,25 @@
+﻿int n = int.Parse(Console.ReadLine());
+bool isFirstArrSelected = true;
+string[] firstArr = new string[n];
+string[] secondArr = new string[n];
+
+for (int i = 0; i < n; i++)
+{
+    string[] numbersArray = Console.ReadLine().Split();
+
+    if (isFirstArrSelected)
+    {
+        firstArr[i] = numbersArray[0];
+        secondArr[i] = numbersArray[1];
+    }
+    else
+    {
+        firstArr[i] = numbersArray[1];
+        secondArr[i] = numbersArray[0];
+    }
+
+    isFirstArrSelected = !isFirstArrSelected;
+}
+
+Console.WriteLine(string.Join(" ", firstArr));
+Console.WriteLine(string.Join(" ", secondArr));
