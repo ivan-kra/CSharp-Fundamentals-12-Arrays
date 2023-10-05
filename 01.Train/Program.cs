9ed207ced65f2b3a1d95806ec223294e414ab9ba
Ivan Kratchanov <ivankra@gmail.com>
2023-10-05 21:35:07 +0300

    Add files via upload

---
+++ 01.Train/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+int n = int.Parse(Console.ReadLine());
+int[] numbers = new int[n];
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++) 
+{
+    numbers[i] = int.Parse(Console.ReadLine());
+    sum += numbers[i];
+}
+
+Console.WriteLine(string.Join(" ", numbers));
+Console.WriteLine(sum.ToString());
