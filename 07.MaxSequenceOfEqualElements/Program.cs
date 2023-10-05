0742ef20d5f8214c604b352986fe43244e3fcb16
Ivan Kratchanov <ivankra@gmail.com>
2023-10-05 21:37:44 +0300

    Add files via upload

---
+++ 07.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] symbols = Console.ReadLine().Split();
+int bestCount = 0;
+string bestCountSymbol = "";
+
+for (int i = symbols.Length - 1; i >= 0; i--)
+{
+    int count = 1;
+    for (int j = i - 1; j >= 0; j--)
+    {
+        if (symbols[i] == symbols[j])
+        {
+            count++;
+            if (bestCount <= count)
+            {
+                bestCount = count;
+                bestCountSymbol = symbols[i];
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+for (int i = 0; i < bestCount; i++)
+{
+    Console.Write($"{bestCountSymbol} ");
+}
