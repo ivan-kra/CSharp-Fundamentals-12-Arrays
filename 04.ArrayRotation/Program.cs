6dff7e4b816e95d4a8ccbdc88bbce82c09b654f1
Ivan Kratchanov <ivankra@gmail.com>
2023-10-05 21:36:55 +0300

    Add files via upload

---
+++ 04.ArrayRotation/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] array = Console.ReadLine().Split();
+int n = int.Parse(Console.ReadLine());
+
+string[] newArray = new string[array.Length];
+
+
+for (int k = 0; k < array.Length; k++)    //или друг начин за копиране на масив: newArray = (string[])array.Clone();
+{
+    newArray[k] = array[k];
+}
+
+
+for (int i = 0; i < n; i++)
+{
+
+    for (int k = 0; k < array.Length; k++)
+    {
+        array[k] = newArray[k];
+    }
+
+    for (int j = 0; j < array.Length - 1; j++)
+    {
+        newArray[j] = array[j + 1];
+        newArray[array.Length - 1] = array[0];
+    }
+}
+
+Console.WriteLine(string.Join(" ", newArray));
---
+++ 05.TopIntegers/Program.cs
@@ -0,0 +1,41 @@
+﻿int[] array = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+for (int i = 0; i < array.Length - 1; i++)
+{
+        if (array[i] > array[i + 1])
+        {
+            Console.Write(array[i] + " ");
+        }
+}
+
+//Console.Write(array[array.Length - 1]);
+
+//string[] inputArr = Console.ReadLine().Split();
+//int[] numbers = new int[inputArr.Length];
+
+//for (int i = 0; i < inputArr.Length; i++)
+//{
+//    numbers[i] = int.Parse(inputArr[i]);
+//}
+
+//// SOLUTION
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    bool isTop = true;
+//    for (int j = i + 1; j < numbers.Length; j++)
+//    {
+//        if (numbers[i] <= numbers[j])
+//        {
+//            isTop = false;
+//            break;
+//        }
+//    }
+
+//    if (isTop)
+//    {
+//        // OUTPUT
+//        Console.Write($"{numbers[i]} ");
+//    }
